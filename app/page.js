d4c5f0afc0f1053f212cc851b9da95c150612c03
Atul Mishra <atulkmishra858@gmail.com>
2024-12-25 23:19:28 +0530

    some ui-changes

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import FaceDetection from '@/components/face-detection'
+
 import React from 'react'
 
 function page() {
   return (
     <div className='flex min-h-screen flex-col items-center p-8'>
-      <h1 className='font-semibold text-3xl md:text-6xl lg:text-8xl bg-gradient-to-r from-gray-600 via-gray-800 to-gray-400 bg-clip-text text-transparent '>
+      <div className='flex gap-5 relative items-center justify-center'>
+      <h1 className='font-semibold text-3xl md:text-6xl lg:text-8xl bg-gradient-to-r from-gray-600 via-gray-900 to-gray-400 bg-clip-text text-transparent '>
         Face Detection 
       </h1>
+     
+      </div>
       <FaceDetection/>
 
     </div>
